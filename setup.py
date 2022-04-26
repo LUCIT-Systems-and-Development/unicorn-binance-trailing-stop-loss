b01df93d345b1e06385c3b439159816da200cf9b
oliver <oliver@zehentleitner.co>
2022-04-26 03:22:57 +0200

    init

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setuptools.setup(
      long_description=long_description,
      long_description_content_type="text/markdown",
      license='MIT License',
-     install_requires=['requests', 'unicorn-binance-websocket-api', 'unicorn-binance-rest-api'],
+     install_requires=['requests', 'unicorn-binance-websocket-api', 'unicorn-binance-rest-api', 'ubtsl'],
      keywords='binance, ',
      project_urls={
         'Documentation': 'https://lucit-systems-and-development.github.io/unicorn-binance-trailing-stop-loss',
