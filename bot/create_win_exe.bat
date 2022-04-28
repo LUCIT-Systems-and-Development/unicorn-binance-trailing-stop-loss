920809f26758870d60006dbf90d20be9b8293cfb
Oliver <oz@lucit.co>
2022-04-28 09:35:38 +0200

    setup windows

--- bot/create_win_exe.bat
+++ bot/create_win_exe.bat
@@ -32,8 +32,8 @@
 :: IN THE SOFTWARE.
 
 echo Installing wheel
-py -m pip install "Z:\unicorn-binance-trailing-stop-loss\dist\unicorn_binance_trailing_stop_loss-0.5.0-py3-none-any.whl" --upgrade
-:: py -m pip install "Z:\unicorn-binance-trailing-stop-loss\dist\unicorn_binance_trailing_stop_loss-0.5.0-py3-none-any.whl" --upgrade --force-reinstall
+:: py -m pip install "Z:\unicorn-binance-trailing-stop-loss\dist\unicorn_binance_trailing_stop_loss-0.5.0-py3-none-any.whl" --upgrade
+py -m pip install "Z:\unicorn-binance-trailing-stop-loss\dist\unicorn_binance_trailing_stop_loss-0.5.0-py3-none-any.whl" --upgrade --force-reinstall
 echo Creating ubtsl.exe ...
 cd bot
 pyinstaller ubtsl.py
