873fdf941f6e8b44f9f1bf3fac6def6f830368a6
oliver <oliver@zehentleitner.co>
2022-04-28 09:29:55 +0200

    fix imports

--- unicorn_binance_trailing_stop_loss/cli.py
+++ unicorn_binance_trailing_stop_loss/cli.py
@@ -34,7 +34,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
-from .manager import BinanceTrailingStopLossManager
+from unicorn_binance_trailing_stop_loss.manager import BinanceTrailingStopLossManager
 from unicorn_binance_rest_api.manager import BinanceRestApiManager, BinanceAPIException
 from configparser import ConfigParser, ExtendedInterpolation
 import argparse
