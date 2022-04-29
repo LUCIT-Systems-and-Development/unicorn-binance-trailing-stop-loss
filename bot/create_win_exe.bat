341c99a6e67977148d481a4020b532d2405a2c3b
Oliver <oz@lucit.co>
2022-04-29 17:08:46 +0200

    install scripts

--- bot/create_win_exe.bat
+++ bot/create_win_exe.bat
@@ -36,7 +36,8 @@ py -m pip install "Z:\unicorn-binance-trailing-stop-loss\dist\unicorn_binance_tr
 :: py -m pip install "Z:\unicorn-binance-trailing-stop-loss\dist\unicorn_binance_trailing_stop_loss-0.5.0-py3-none-any.whl" --upgrade --force-reinstall
 echo Creating ubtsl.exe ...
 cd bot
+echo Createing ubtsl.exe
 pyinstaller ubtsl.py
-robocopy dist Z:\unicorn-binance-trailing-stop-loss\bot\ /s /e
+robocopy dist Z:\unicorn-binance-trailing-stop-loss\dist\ /s /e
 cd ..
 bot\remove_win_exe.bat
